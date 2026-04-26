5e7f8303bbf1a63e60bd3040c63c4979c65846f1
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-26 23:58:26 +0530

    project

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -9,3 +9,14 @@ resource "aws_instance" "terraform" {
       }
     )
 }
+
+resource "aws_security_group" "allow_all" {
+  name   = "${local.common_name}-workspace"
+
+  egress {
+    from_port        = var.egress_from_port
+    to_port          = var.egress_to_port
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = var.cidr
+  }
+
