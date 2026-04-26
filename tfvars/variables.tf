eda0a7dba2e71f1a953ac78fafdf21295527dc48
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-26 23:59:45 +0530

    project

--- tfvars/variables.tf
+++ tfvars/variables.tf
@@ -7,3 +7,14 @@ variable "environment" {
     type = string
 }
 
+variable "ami_id" {
+    type = string
+    default     = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+    type = string
+}
+
+
+variable "cidr" {
